c42af8523a457dcafa55d363109568e6478633af
AlessandroSilva <alessandro_hudson@hotmail.com>
2020-01-28 14:41:53 -0300

    Criacao repositorio

---
+++ TictacToe/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace TictacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 9);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(486, 153);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 175);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(158, 142);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(176, 175);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(158, 142);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(340, 175);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(158, 142);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 323);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(158, 142);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(176, 323);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(158, 142);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(340, 323);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(158, 142);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(12, 471);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(158, 142);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(176, 471);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(158, 142);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(340, 471);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(158, 142);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 631);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Novo Jogo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 660);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TictacToe/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TictacToe
+{
+    public partial class Form1 : Form
+    {
+        string[,] matriz;
+        string Jogada;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarJogo();
+        }
+
+        private void CarregarJogo()
+        {
+            matriz = new string[3, 3];
+            lblResultado.Text = "É a vez de X";
+
+            foreach (var b in this.Controls.OfType<Button>())
+            {
+                b.Enabled = true;
+                b.Text = string.Empty;
+            }
+
+            Jogada = string.Empty;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if(Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn1.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn1.Text = "O";
+            }
+
+            btn1.Enabled = false;
+            matriz[0, 0] = btn1.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn2.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn2.Text = "O";
+            }
+
+            btn2.Enabled = false;
+            matriz[0, 1] = btn2.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn3.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn3.Text = "O";
+            }
+
+            btn3.Enabled = false;
+            matriz[0, 2] = btn3.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn4.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn4.Text = "O";
+            }
+
+            btn4.Enabled = false;
+            matriz[1, 0] = btn4.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn5.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn5.Text = "O";
+            }
+
+            btn5.Enabled = false;
+            matriz[1, 1] = btn5.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn6.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn6.Text = "O";
+            }
+
+            btn6.Enabled = false;
+            matriz[1, 2] = btn6.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn7.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn7.Text = "O";
+            }
+
+            btn7.Enabled = false;
+            matriz[2, 0] = btn7.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn8.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn8.Text = "O";
+            }
+
+            btn8.Enabled = false;
+            matriz[2, 1] = btn8.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (Jogada == string.Empty || Jogada == "O")
+            {
+                Jogada = "X";
+                btn9.Text = "X";
+            }
+            else
+            {
+                Jogada = "O";
+                btn9.Text = "O";
+            }
+
+            btn9.Enabled = false;
+            matriz[2, 2] = btn9.Text;
+            VerificarJogo(matriz);
+        }
+
+        private void VerificarJogo(string[,] matriz)
+        {
+            var resultadoHorizontal = VerificarJogadaHorizontal(matriz);
+            var resultadoVertical = VerificarJogadaVertical(matriz);
+            var resultadoDiagonal = VerificarJogadaDiagonal(matriz);
+
+            if (resultadoHorizontal.ToString() == string.Empty && resultadoVertical.ToString() == string.Empty && resultadoDiagonal.ToString() == string.Empty)
+            {
+                if (Jogada == "X")
+                {
+                    lblResultado.Text = "E a vez de O jogar";
+                }
+                else
+                {
+                    lblResultado.Text = "E a vez de X jogar";
+                }
+            }
+        }
+
+        private object VerificarJogadaDiagonal(string[,] matriz)
+        {
+            if (matriz[0, 0] == "X" && matriz[1, 1] == "X" && matriz[2, 2] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[0, 0] == "O" && matriz[1, 1] == "O" && matriz[2, 2] == "O")
+            {
+                return lblResultado.Text = "O ganhou!!";
+            }
+            else if (matriz[2, 0] == "X" && matriz[1, 1] == "X" && matriz[0, 2] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[2, 0] == "O" && matriz[1, 1] == "O" && matriz[0, 2] == "O")
+            {
+                return lblResultado.Text = "O ganhou!!";
+            }
+
+            return string.Empty;
+        }
+
+        private object VerificarJogadaVertical(string[,] matriz)
+        {
+            if (matriz[0, 0] == "X" && matriz[1, 0] == "X" && matriz[2, 0] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[0, 0] == "O" && matriz[1, 0] == "O" && matriz[2, 0] == "O")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[0, 1] == "X" && matriz[1, 1] == "X" && matriz[2, 1] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[0, 1] == "O" && matriz[1, 1] == "O" && matriz[2, 1] == "O")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[0, 2] == "X" && matriz[1, 2] == "X" && matriz[2, 2] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[0, 2] == "O" && matriz[1, 2] == "O" && matriz[2, 2] == "O")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+
+            return string.Empty;
+        }
+
+        private object VerificarJogadaHorizontal(string[,] matriz)
+        {
+            if (matriz[0, 0] == "X" && matriz[0, 1] == "X" && matriz[0, 2] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[0, 0] == "O" && matriz[0, 1] == "O" && matriz[0, 2] == "O")
+            {
+                return lblResultado.Text = "O ganhou!!";
+            }
+            else if (matriz[1, 0] == "X" && matriz[1, 1] == "X" && matriz[1, 2] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[1, 0] == "O" && matriz[1, 1] == "O" && matriz[1, 2] == "O")
+            {
+                return lblResultado.Text = "O ganhou!!";
+            }
+            else if (matriz[2, 0] == "X" && matriz[2, 1] == "X" && matriz[2, 2] == "X")
+            {
+                return lblResultado.Text = "X ganhou!!";
+            }
+            else if (matriz[2, 0] == "O" && matriz[2, 1] == "O" && matriz[2, 2] == "O")
+            {
+                return lblResultado.Text = "O ganhou!!";
+            }
+
+            return string.Empty;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            CarregarJogo();
+        }
+    }
+}
